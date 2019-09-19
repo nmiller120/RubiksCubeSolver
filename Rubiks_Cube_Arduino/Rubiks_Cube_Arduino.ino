90792ae24d1bfd61aaac40c8e18a3e893af37421
Nick Miller <nick.miller@cspplastics.com>
2019-09-19 17:54:30 -0400

    updated comments in arduino code

--- Rubiks_Cube_Arduino/Rubiks_Cube_Arduino.ino
+++ Rubiks_Cube_Arduino/Rubiks_Cube_Arduino.ino
@@ -1,6 +1,6 @@
  /**************************************************************************
  Code for the firmware running on the arduino for the rubiks cube project. The arduino acts as a controller for the assembly, taking commands from the PC. The assembly begins in standby mode where 
- it waits for a command...
+ it waits for a command.
 
   - In "Program" mode the arduino waits for the default positions of each of the servo motors to be loaded into the settingArray
   - In "calibration" mode the arduino accepts a continuous stream of commands for the positons of each of the servo motors. Unlike in run mode these positions are a value between 0 and 180 allowing the servo motors to
@@ -18,10 +18,11 @@
  */
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
-
+s
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
+// constants defined to use in the calculation of the pulse width sent to the servo motor driver
 #define MIN_PULSE_WIDTH       650
 #define MAX_PULSE_WIDTH       2350
 #define DEFAULT_PULSE_WIDTH   1500
@@ -37,12 +38,13 @@ int servoPos;  // servo position, use in run
 char readByte;
 
 
-int settingArray[8][6]; // ""<wZW¬&nZS 8uZX­-oZW¦""
+int settingArray[8][6]; 
 // settingArray: row is servo, col is position, even rows are linear, odd rows are radial
 // linear goes 45 pos, 135 pos, 45+ pos, 135+ pos, dummy 90
 // radial goes 0 pos, 90 pos, 180 pos, 0+ pos, 90+ pos
 
 void setup(){
+  // open serial port, begin communication with the servo driver
   Serial.begin(9600);
   Serial.print(mode);
   pwm.begin();
@@ -50,7 +52,7 @@ void setup(){
 }
 
 void loop() {
-
+  // main loop, 4 different modes of operation
   while (Serial.available()==0){}// wait for serial data
   if (mode == 2) {run_mode();} // if in run, run run 
   else if (mode == 3) {calibration();} // if in calibration mode, run calibration
@@ -59,6 +61,12 @@ void loop() {
 }
 
 void calibration(){
+  /*
+     Function reads a semi-continuous stream of data. Each calibration command is coded in ASCII text and is of the form
+     1.x.x.x.x.x.x where 1 signals the start of a new command and each x is an integer between 0 and 180. The following code 
+     parses those commands and writes them out to the motor controller using the setPWM() function. If a value of -10 is sent to the 
+     arduino the assembly returns to standby mode
+  */
   while(Serial.available()){
     int readIN = Serial.parseInt();
     if (readIN == 1){
@@ -73,6 +81,15 @@ void calibration(){
   }
 
 void run_mode(){
+  /*    
+    Function reads coded commands from the serial port and moves the motors to match the described positions. Motors 
+    That aren't addressed in the command don't move. If the arduino recieves an invalid input the program returns to standby
+     
+    run mode serial commands: first character refers to the servo to control,
+    2nd character refers to the position, ie, "0121" means servo 0 position 1, servo 2
+    position 1. The positions are defined during the program phase, which is implemented
+    in the code for the __init__() method of this class
+  */
   int servoNumber;
   int servoPosition;
 
@@ -93,6 +110,18 @@ void run_mode(){
   }
   
 void program(){
+  /*
+   * This function is how the default position values are loaded to the arduino. The software running on the PC sends the positioning data as a stream of 
+   * bytes (ie. 180 is transmitted as a single byte with hex value 0xB4). These values are loaded into the settingArray. The setting array is a 2d array. Even 
+   * rows in the array are controlling linear motion, odd rows in the array are controlling radial motion. 
+   * 
+   * Format of linear = [deg45, deg135, 0, 0, 0, 90] // the 90 degree values act as an error check of the instruction
+   * Format of radial = [deg0, deg90, deg180, 0, deg90+, deg180+] deg90+ and deg180+ are slightly more agressive turns to help allignment of the cube face when turning a 
+   * side.
+   * 
+   * Automatically goes back to standby mode after program settings are recieved
+   */
+  
   while (Serial.available()){
       delay(1000); //give the buffer time to fill
       for (int i=0; i<8; i++){
@@ -121,6 +150,11 @@ void program(){
 }
 
 void standby(){
+  /*
+   * Standby mode waits for a mode change command. Command is a 5 character ascii string. Debug mode can also be toggled in standby. This just sends debug messsages
+   * to the serial port along with the acnowlegments that are sent when debug mode is off. 
+   */
+  
   char modeSelect[8] = "";
       Serial.readBytes(modeSelect, 5);
       if (!strcmp(modeSelect, "progm")){changeMode(1);} // set to program mode if "program" was recieved in the serial buffer
@@ -144,6 +178,9 @@ void standby(){
 }
 
 void checkData(){
+  /*
+   * If a check command is recieved in standby mode, this function will write the currently stored settingArray to the serial port
+   */
   Serial.println("Recieved data..."); 
   for (int i=0; i<8; i++){
     for (int j=0; j<6; j++){
@@ -155,6 +192,7 @@ void checkData(){
   }
 
 void changeMode(int newMode){
+  // function invoked when a changemode command is recieved at the serial port
   mode = newMode;
   if (debug == true) {
     Serial.print("In ");
@@ -165,9 +203,9 @@ void changeMode(int newMode){
   }
 
 int pulseWidth(int angle){
+  // calculates the pulse width value needed to achieve the desired angle of the servo motor
   int pulse_wide, analog_value;
   pulse_wide   = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
   analog_value = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
-  //Serial.println(analog_value);
   return analog_value;
 }
